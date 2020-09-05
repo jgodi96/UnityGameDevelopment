82ee721abf72f672f74184f080b3d9ad34951d98
Joshua Godinez <jgodi96@gmail.com>
2020-09-05 15:40:13 -0700

    Added collision

--- 3d_Project_RocketBoost/Assets/Rocket.cs
+++ 3d_Project_RocketBoost/Assets/Rocket.cs
@@ -5,38 +5,78 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {                                           // act on RigidBody Components in GUI
-        rigidBody = GetComponent<Rigidbody>(); //can get a component and work on multiple components
+        rigidBody = GetComponent<Rigidbody>();//can get a component and work on multiple components
+        audioSource = GetComponent<AudioSource>();
         
     }
 
     // Update is called once per frame
     void Update()
-    {
-        ProcessInput();
+    {   Thrust();
+        Rotate();     
     }
 
-    private void ProcessInput()
+    void OnCollisionEnter(Collision collision)
     {
+       switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
 
+        }
+
+    }
+
+    private void Thrust()
+    {
+        
         if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);//if angeled, always thrusts towards the top
-            print("Thrusting");
+
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);//if angeled, always thrusts towards the top
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of rotation
+        float rotationThisFrame = rcThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward); //goes positive on z axis //anticlockwise
+            
+            transform.Rotate(Vector3.forward  * rotationThisFrame); //goes positive on z axis //anticlockwise
         }
         else if (Input.GetKey(KeyCode.D)) //cant rotate while clicking A
         {
-            print("Rotating Right");
+       
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
-      
-        
+        rigidBody.freezeRotation = false; //resume to physics control
+
     }
 }
