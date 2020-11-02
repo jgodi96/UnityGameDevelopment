f5ca17d481127a652946e446b3f838820b0f010c
Joshua Godinez <jgodi96@gmail.com>
2020-11-01 20:04:35 -0800

    Refactored translation

--- 3D_Project_RailShooter/Assets/Player.cs
+++ 3D_Project_RailShooter/Assets/Player.cs
@@ -5,8 +5,9 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In ms^-1")][SerializeField] float speed = 20f;
     [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +16,20 @@ public class Player : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+    { 
+        //print(horizontalThrow);
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
 
         float rawXPos = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-        transform.localPosition = new Vector3(clampedXPos,transform.localPosition.y,transform.localPosition.z);
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos,clampedYPos,transform.localPosition.z);
     }
 }
